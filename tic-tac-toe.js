c90cefc2bded4b837c9e92953d792e086d4ae03f
Kristina Đurek <kdurek@inceptum.hr>
2019-02-11 16:44:44 +0100

    sad funkcioniše al nije gotovo

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,61 +1,57 @@
-
 let turn = "X";
 let result = false;
 winner = null;
 
-function startGame () {
-    setMessage(turn + " Gets to start")
+function startGame() {
+  setMessage(turn + " Gets to start");
 }
-function setMessage(msg){
-    document.getElementById("message").innerText = msg;
+function setMessage(msg) {
+  document.getElementById("message").innerText = msg;
 }
 function nextMove(square) {
-    if (winner != null){
-        setMessage(turn + " already won.")
-    }else if(square.innerText == '') {
-        square.innerText = turn;
-        switchTurn();
-    }
-    else {
-        setMessage("Pick another square")
-    }
-   
+  if (winner != null) {
+    setMessage(turn + " already won.");
+  } else if (square.innerText == "") {
+    square.innerText = turn;
+    switchTurn();
+  } else {
+    setMessage("Pick another square");
+  }
 }
 function switchTurn() {
-    if(checkForWinner(turn)){
-        setMessage("Congrats " + turn + ", you won!")
-    }else if(turn == "X") {
-    turn = "0";
-    setMessage("It's " + turn + "' turn")
+  if (checkForWinner(turn)) {
     winner = turn;
-}
-else {
+    setMessage("Congrats " + turn + ", you won!");
+  } else if (turn == "X") {
+    turn = "0";
+    setMessage("It's " + turn + "' turn");
+  } else {
     turn = "X";
-    setMessage("It's " + turn + "' turn")
-    }
+    setMessage("It's " + turn + "' turn");
+  }
 }
 function checkForWinner(move) {
-    if (
-      checkRow(1, 2, 3, move) ||
-      checkRow(4, 5, 6, move) ||
-      checkRow(7, 8, 9, move) ||
-      checkRow(1, 4, 7, move) ||
-      checkRow(2, 5, 8, move) ||
-      checkRow(3, 6, 9, move) ||
-      checkRow(1, 5, 9, move) ||
-      checkRow(3, 5, 7, move)
-    ) {
-      result = true;
-    }
-    return result;
-  }
-function checkRow(a,b,c, move){
-if (getBox(a) == move && getBox(b) == move && getBox(c) == move){
+  if (
+    checkRow(1, 2, 3, move) ||
+    checkRow(4, 5, 6, move) ||
+    checkRow(7, 8, 9, move) ||
+    checkRow(1, 4, 7, move) ||
+    checkRow(2, 5, 8, move) ||
+    checkRow(3, 6, 9, move) ||
+    checkRow(1, 5, 9, move) ||
+    checkRow(3, 5, 7, move)
+  ) {
     result = true;
+  }
+  return result;
 }
-return result; 
+function checkRow(a, b, c, move) {
+  if (getBox(a) == move && getBox(b) == move && getBox(c) == move) {
+    result = true;
+  }
+  return result;
 }
 
-function getBox(number){
-    return document.getElementById("s" + number).innerText;
+function getBox(number) {
+  return document.getElementById("s" + number).innerText;
 }
