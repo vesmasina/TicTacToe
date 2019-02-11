6f647348909fd97b157c8f858cceb254002eb925
Kristina Đurek <kdurek@inceptum.hr>
2019-02-11 14:52:04 +0100

    zasto nece radit

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,3 +1,4 @@
+
 function startGame () {
     turn = "X";
     setMessage(turn + " Gets to start")
@@ -16,9 +17,38 @@ function nextMove(square) {
    
 }
 function switchTurn() {
-if(turn === "X") {
+    if(checkForWinner(turn)){
+        setMessage("Congrats " + turn + ", you won!")
+    }
+else if(turn === "X") {
     turn = "0";
+    setMessage("It's " + turn + "' turn")
 }
 else {turn = "X";
+    setMessage("It's " + turn + "' turn")
     }
+    
+}
+function checkForWinner(move){
+    let result = false;
+if(checkRow(1,2,3, move)) ||
+    (checkRow(4,5,6, move)) ||
+    (checkRow(7,8,9, move)) ||
+    (checkRow(1,4,7, move)) ||
+    (checkRow(2,5,8, move)) ||
+    (checkRow(3,6,9, move)) ||
+    (checkRow(1,5,9, move)) ||
+    (checkRow(3,5,7, move)) {
+        result = true;
+    }
+    return result;
+}
+function checkRow(a,b,c, move){
+let result = false;
+if (getBox(a)==move && getBox(b) == move && getBox(c)== move){
+    result = true;
+}
+}
+function getBox(){
+    return document.getElementById("s" + number).innerText;
 }
